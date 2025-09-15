b284c19ef98032e660db7f32bb6402084c84680e
Angela Yuan <ayuan1@hwemail.com>
2025-09-15 12:58:46 -0700

    Creates directory and text files.
    Initializes a directory named "JavaFileSystem" and three text files (notes.txt, data.txt, and log.txt) within it. This sets up the
    basic file structure for the application.

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -1,14 +1,19 @@
 import java.io.*;
 
 public class FileHandlingActivity {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Your code here
         
         // a. Create main directory
         File dir = new File("JavaFileSystem");
         dir.mkdir();
         // b. Create three text files
-
+        File file1 = new File("JavaFileSystem/notes.txt");
+        file1.createNewFile();
+        File file2 = new File("JavaFileSystem/data.txt");
+        file2.createNewFile();
+        File file3 = new File("JavaFileSystem/log.txt");
+        file3.createNewFile();
         // c. Write messages to files
         
         // d. Read and display file contents
